967db9af1ea57f19256d5e618b06a4cb3d3a3a3a
rashmishah346@gmail.com <rashmishah346@gmail.com>
2026-08-13 21:29:18 +0530

    Added gmail api

---
+++ backend/src/utils/mail.js
@@ -0,0 +1,41 @@
+import { google } from "googleapis";
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oAuth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
+
+const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
+
+function encodeMessage(to, subject, html) {
+  const messageParts = [
+    `From: "Dev Services" <${process.env.EMAIL_USER}>`,
+    `To: ${to}`,
+    "Content-Type: text/html; charset=utf-8",
+    "MIME-Version: 1.0",
+    `Subject: ${subject}`,
+    "",
+    html,
+  ];
+  const message = messageParts.join("\n");
+
+  return Buffer.from(message)
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+export const sendEmail = async (to, subject, html) => {
+  const raw = encodeMessage(to, subject, html);
+
+  await gmail.users.messages.send({
+    userId: "me",
+    requestBody: { raw },
+  });
+};
