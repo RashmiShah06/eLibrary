2b88e9b7c58cee6e14baa28c38cf9a29b114c6c0
rashmishah346@gmail.com <rashmishah346@gmail.com>
2026-08-14 03:52:22 +0530

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,33 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import userRoutes from "./routes/user.route.js";
 import bookRoutes from "./routes/book.route.js";
+import borrowRoutes from "./routes/borrow.route.js";
+import reservationRoutes from "./routes/reservation.route.js";
 
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
+app.use("/api/borrow", borrowRoutes);
+app.use("/api/reservations", reservationRoutes);
 
 export default app;
