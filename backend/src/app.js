bc25b5407dc1072f02611eb87d025cd616531a6c
rashmishah346@gmail.com <rashmishah346@gmail.com>
2026-08-13 20:29:06 +0530

    Add user routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
-export default app;
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
